fa27acf4da25018d8597e32cc613c3af4f7df052
jailsonsf <jailsonsoares002@gmail.com>
2019-10-09 09:43:53 -0300

    Adiciona exercícios :rocket: Exercícios feitos em C++ adicionados no diretório Problemas/C++/

---
+++ Problemas/C++/Divisao.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double A, B;
+    cin >> A >> B;
+
+    cout.precision(2);
+    cout.setf(ios::fixed);
+    cout << A / B << "\n" << endl;
+
+    return 0;
+}
---
+++ Problemas/C++/Soma.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int X, Y;
+    cin >> X >> Y;
+
+    cout << X + Y << "\n" << endl;
+
+    return 0;
+}
