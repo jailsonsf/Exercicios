20f2c37ec02f297b6bdc8fbe97734b2e7189b7fd
jailsonsf <jailson.s.f@outlook.com>
2019-10-13 21:45:07 -0300

    Adiciona exercício :rocket:
    Exercício Quadrado Mágico adiciona, solução ainda possui erro em tempo de execução

---
+++ ProgramacaoBasicaCpp/exercícios/QuadradoMagico.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int len, sum, vertSum, horiSum, diagoSum;
+    cin >> len;
+
+    int magicSquare[len][len];
+
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < len; j++) {
+            cin >> magicSquare[i][j];
+
+        }
+    }
+
+    for (int i = 0; i < len; i++) {
+        horiSum = 0;
+        vertSum = 0;
+        diagoSum = 0;
+        for (int j = 0; j < len; j++) {
+            horiSum += magicSquare[i][j];
+            vertSum += magicSquare[j][i];
+            diagoSum += magicSquare[j][j];
+
+        }
+
+        if (i == 0)
+            sum = horiSum;
+
+    }
+
+    if (sum == vertSum && sum == diagoSum)
+        cout << sum << "\n";
+
+    return 0;
+}
