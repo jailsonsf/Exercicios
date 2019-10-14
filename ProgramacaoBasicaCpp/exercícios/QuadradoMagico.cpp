4beadb90baa967205470a525c5f8fa55ccdf0348
jailsonsf <jailson.s.f@outlook.com>
2019-10-14 17:29:22 -0300

    Atualiza exercício

--- ProgramacaoBasicaCpp/exercícios/QuadradoMagico.cpp
+++ ProgramacaoBasicaCpp/exercícios/QuadradoMagico.cpp
@@ -9,30 +9,32 @@ int main() {
     int magicSquare[len][len];
 
     for (int i = 0; i < len; i++) {
-        for (int j = 0; j < len; j++) {
-            cin >> magicSquare[i][j];
-
-        }
-    }
-
-    for (int i = 0; i < len; i++) {
         horiSum = 0;
         vertSum = 0;
         diagoSum = 0;
         for (int j = 0; j < len; j++) {
+            cin >> magicSquare[i][j];
+
             horiSum += magicSquare[i][j];
             vertSum += magicSquare[j][i];
             diagoSum += magicSquare[j][j];
 
         }
 
-        if (i == 0)
+        if (i == 0) {
             sum = horiSum;
+
+        }
 
     }
 
-    if (sum == vertSum && sum == diagoSum)
+    if (sum == vertSum && sum == diagoSum) {
         cout << sum << "\n";
+
+    } else {
+        cout << "-1" << "\n";
+
+    }
 
     return 0;
 }
